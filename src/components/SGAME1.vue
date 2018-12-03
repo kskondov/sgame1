b4aac59f027907a72ce0a00c226e37942cebbee9
Simeto <simeonps@abv.bg>
2018-12-03 11:31:08 +0200

    Add phaser

---
+++ src/components/SGAME1.vue
@@ -0,0 +1,55 @@
+<template>
+  <div :id="id">
+  </div>
+</template>
+
+<script>
+import Phaser from 'phaser'
+
+export default {
+  name: 'SGAME1',
+  data () {
+    return {
+      id: null,
+      game: null
+    }
+  },
+  props: {
+    msg: String
+  },
+  beforeMount () {
+    this.id = 'game' + this._uid
+  },
+  mounted () {
+    var config = {
+      type: Phaser.CANVAS,
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 200 }
+        }
+      }
+    }
+    this.game = new Phaser.Game(config)
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Phaser from 'phaser'
 
 Vue.config.productionTip = false
+
+Vue.use(Phaser)
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
